184e6bdc0bd1529ecf5105ee7cc3153cab27c7e9
Santiago Salazar <santiagosalazar.dev@gmail.com>
2023-04-02 22:40:54 -0400

    Añadido form con efecto face en js

--- web/app/themes/galena/resources/assets/scripts/routes/common.js
+++ web/app/themes/galena/resources/assets/scripts/routes/common.js
@@ -2,7 +2,55 @@ import {Solar} from '../solar';
 
 export default {
   init() {
-  },
+    // btn fade
+    let nl = document.querySelector("[data-newsletter]");
+    let appear = document.querySelector("[data-newsletter-appear]");
+    let desappear = document.querySelector("[data-newsletter-desappear]");
+
+    appear.addEventListener("click", (e) => {
+      nl.classList.add("_newsletter-animation");
+    });
+
+    desappear.addEventListener("click", (e) => {
+      e.preventDefault();
+      nl.classList.remove("_newsletter-animation");
+
+      setTimeout(() => {
+        let _inputName = document.querySelector("[type=text]").value = "";
+        let _inputEmail = document.querySelector("[type=email]").value = "";
+      }, 600);
+    });
+
+    // data submit
+    let _btnSubmit = document.querySelector("[type=submit]");
+
+    const upload = (arg) => {
+      fetch('', {
+        method: 'POST',
+        body: arg
+      }).then(
+        response => response.json()
+      ).then(
+        success => console.log(success)
+      ).catch(
+        error => console.log(error)
+      );
+    };
+
+    _btnSubmit.addEventListener("click", function(e){
+      e.preventDefault();
+      var _inputName = document.querySelector("[type=text]").value;
+      var _inputEmail = document.querySelector("[type=email]").value;
+
+      var _message = new FormData();
+      _message.append("name", _inputName);
+      _message.append("name", _inputEmail);
+
+      upload(_message);
+    });
+    },
+    
+
   finalize() {
     Solar.start();
   },
--- web/app/themes/galena/resources/views/partials/footer.blade.php
+++ web/app/themes/galena/resources/views/partials/footer.blade.php
@@ -1,4 +1,44 @@
 <footer class="hero is-dark is-medium">
+  <div class="is-flex is-align-items-center" data-newsletter>
+  
+    <div style="background: radial-gradient(circle, #382515 -70%, transparent 40%); opacity: 0.9; width: 100%; height: 100%; position: absolute;"></div>
+    <form class="formularie p-6" action="">
+      <div class="field">
+        <h2 class="subtitle is-uppercase has-text-centered">Suscribe!!</h2>
+
+        <label class="label has-text-light">Name</label>
+        <div class="control has-icons-left">
+          <input class="input" type="text" placeholder="Text input">
+          <span class="icon is-small is-left">
+            <i class="icon has-text-black">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+            </i>
+          </span>
+        </div>
+      </div>
+      
+      <div class="field">
+        <label class="label has-text-light">Email</label>
+        <div class="control has-icons-left">
+          <input class="input" type="email" placeholder="Email input" value="">
+          <span class="icon is-small is-left">
+            <i class="icon has-text-black">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+            </i>
+          </span>
+        </div>
+      </div>
+      
+      <div class="field is-grouped justify-center mt-5">
+        <div class="control">
+          <button class="button is-primary is-hovered" type="submit" data-gravity data-cursor-stick>Register</button>
+        </div>
+        <div class="control">
+          <button class="button is-danger is-light is-hovered" data-gravity data-cursor-stick data-newsletter-desappear>Cancel</button>
+        </div>
+      </div>
+    </form>
+  </div>
   <div class="hero-body">
     <div class="container is-fullhd">
       <div class="_header-footer columns align-items-center is-centered px-6 mx-auto pb-6 mb-6">
@@ -9,15 +49,17 @@
         </a>
         <div class="column is-3 is-offset-1 p-0">
           @set($newsletter_link, get_field('newsletter_link', 'options'))
-          <a href="{{$newsletter_link['url']}}" class="about is-flex is-align-items-center has-text-light justify-center" data-gravity data-cursor-stick>
+          <div href="" class="about is-flex is-align-items-center has-text-light justify-center" data-gravity data-cursor-stick data-newsletter-appear>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle">
                   <circle cx="12" cy="12" r="10"></circle>
               </svg>
               <span class="has-text-light is-capitalized is-size-6 mx-4">
                 {{$newsletter_link['title']}}
               </span>
-          </a>
+            </div>
         </div>
+
+
         <div class="columns column is-5 is-offset-1 p-0">
           <div class="column hast-text-light">
             @set($terms_and_conditions, get_field('terms_and_conditions', 'options'))
