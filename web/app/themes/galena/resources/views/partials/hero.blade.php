a0ee6ea9573a9592a74846412bd06c83390e8806
Santiago Salazar <santiagosalazar.dev@gmail.com>
2023-04-11 15:50:57 -0400

    adding dependencies

--- web/app/themes/galena/resources/views/partials/hero.blade.php
+++ web/app/themes/galena/resources/views/partials/hero.blade.php
@@ -1,9 +1,9 @@
 <div data-cursor="-inverse" class="hero is-fullheight is-clipped is-relative">
     {{-- CONTAINER PARA GALENA E IMG DE GALENA --}}
     <div class="_container-galena" data-inertia data-inertia-speed="-4">
-      <img src="{!!get_field('foto_de_galena', 'options')!!}" alt="Galena AI" data-inertia data-inertia-reveal="1200"> 
+      <img src="{!!get_field('foto_de_galena', 'options')!!}" alt="Galena AI" data-inertia data-inertia-reveal="1200" data-cursor="-flare"> 
     </div>
-
+    
     {{-- EFECTO DE SOMBRA INFERIOR PARA COMPLEMENTAR LA SECCION 2 --}}
     <div class="_sombra-inferior"></div>
 
